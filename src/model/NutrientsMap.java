a0df6bba6cb9aebe82a3c243cf167a11dff4d471
hulsbo <oskar.huledal@simonsoft.se>
2024-06-15 21:02:02 +0200

    nutrientsMap should not allow add/remove of keys

--- src/model/NutrientsMap.java
+++ src/model/NutrientsMap.java
@@ -6,7 +6,6 @@ public class NutrientsMap extends AbstractMap<String, Double> {
     private final Map<String, Double> nutrientsMap = new HashMap<>();
 
     public NutrientsMap() {
-        // Values are non zero for testing purposes.
         nutrientsMap.put("protein", 5.0);
         nutrientsMap.put("fat", 5.0);
         nutrientsMap.put("carbs", 5.0);
@@ -22,6 +21,22 @@ public class NutrientsMap extends AbstractMap<String, Double> {
 
     @Override
     public Double put(String key, Double value) {
-        return nutrientsMap.put(key, value);
+        if (nutrientsMap.containsKey(key)) {
+            nutrientsMap.put(key, value);
+        } else {
+            throw new IllegalArgumentException("Invalid key: " + key);
+        }
+        return value;
     }
+
+    @Override
+    public Double remove(Object key) {
+        throw new UnsupportedOperationException("Keys cannot be removed in a nutrientsMap.");
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("A nutrientsMaps cannot be cleared.");
+    }
+
 }
