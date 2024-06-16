33cfdf794f0378a61e21d8be94ee8d5e3eb198f0
hulsbo <oskar.huledal@simonsoft.se>
2024-06-16 16:52:20 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
 import model.Adventure;
-import model.BaseClass;
-import model.Ingredient;
 import model.Meal;
 
 import java.util.Scanner;
+import java.util.UUID;
 
-import static util.Gender.FEMALE;
-import static util.Gender.MALE;
-import static util.PhysicalActivity.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Meal testMeal = new Meal();
+        Adventure testAdventure = new Adventure();
 
         // testMeal.addCrewMember("Oskar Huledal", 35, 186, 75, MALE, HEAVY);
         // testMeal.addCrewMember("Lovisa Huledal", 30, 180, 70, FEMALE, MODERATE);
@@ -22,41 +18,38 @@ public class Main {
         // testMeal.showMembersDailyKCalNeed();
 
 
+        for (;;) {
+            System.out.println("Enter name of Adventure:");
+            String name = input.nextLine();
+            UUID key = testAdventure.putChild(new Meal());
+            testAdventure.setName(name);
+            System.out.println("Enter name of Meal:");
+            String mealName = input.nextLine();
+            testAdventure.setChildName(key, mealName);
+            testAdventure.printChildren();
+            testAdventure.removeChild(mealName);
+        }
+
+
         // Testing the absolute value map for meals' ingredients
-        for (;;) {
-            System.out.print("Type the name of the ingredient: ");
-            input.nextLine();
-            String name = input.nextLine();
-            Ingredient newIngredient = new Ingredient();
-           testMeal.addIngredient(newIngredient);
-            newIngredient.setName(name);
-            System.out.print("Type the weight of the ingredient: ");
-            double weight = input.nextDouble();
-            testMeal.modifyWeightOfIngredient(newIngredient.getId(), weight);
-           testMeal.printEntries();
-
-        }
-
-        for (;;) {
-
-            testMeal.addIngredient(new Ingredient());
-            System.out.print("Type the name of the meal: ");
-            String name = input.nextLine();
-            testMeal.printEntries();
-            testMeal.setNutrientsMap();
-            System.out.println(testMeal.getNutrientsMap());
-
-            System.out.print("Type the name of the meal: ");
-            String name2 = input.nextLine();
-            testMeal.addIngredient(new Ingredient());
-            testMeal.printEntries();
-
-            System.out.print("Type the name of the meal to remove: ");
-            String removeString = input.nextLine();
-            testMeal.removeEntry(removeString);
-            testMeal.printEntries();
-
-        }
+//        for (;;) {
+//            testMeal.putChild(new Ingredient());
+//            System.out.print("Type the name of the meal: ");
+//            String name = input.nextLine();
+//            testMeal.printEntries();
+//            testMeal.setNutrientsMap();
+//            System.out.println(testMeal.getNutrientsMap());
+//
+//            System.out.print("Type the name of the meal: ");
+//            String name2 = input.nextLine();
+//            testMeal.addIngredient(new Ingredient());
+//            testMeal.printEntries();
+//
+//            System.out.print("Type the name of the meal to remove: ");
+//            String removeString = input.nextLine();
+//            testMeal.removeEntry(removeString);
+//            testMeal.printEntries();
+//        }
 
     }
 }
