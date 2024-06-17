ce64cb105bb157c283f011e6c578fcdd45dabcef
hulsbo <oskar.huledal@simonsoft.se>
2024-06-17 17:42:00 +0200

    change local to US

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ import java.util.UUID;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
         Adventure testAdventure = new Adventure();
         Meal testMeal = new Meal();
